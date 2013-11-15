c0ebc33cd7483f0aaee256dbe0c9bbffe6bc90ce
htchoi1995 <htchoi1995@gmail.com>
2013-11-16 02:42:18 +0900

    build: Make completely with thinkingbridge
    PS1. Rename CM -> THINKINGBRIDGE
    PS2. Remove CM secure
    PS3. fix stuff
    
    build works?

--- envsetup.sh
+++ envsetup.sh
@@ -73,13 +73,13 @@ function check_product()
         return
     fi
 
-    if (echo -n $1 | grep -q -e "^cm_") ; then
-       CM_BUILD=$(echo -n $1 | sed -e 's/^cm_//g')
-       export BUILD_NUMBER=$((date +%s%N ; echo $CM_BUILD; hostname) | openssl sha1 | sed -e 's/.*=//g; s/ //g' | cut -c1-10)
+    if (echo -n $1 | grep -q -e "^thinkingbridge_") ; then
+       THINKINGBRIDGE_BUILD=$(echo -n $1 | sed -e 's/^thinkingbridge_//g')
+       export BUILD_NUMBER=$((date +%s%N ; echo $THINKINGBRIDGE_BUILD; hostname) | openssl sha1 | sed -e 's/.*=//g; s/ //g' | cut -c1-10)
     else
-       CM_BUILD=
+       THINKINGBRIDGE_BUILD=
     fi
-    export CM_BUILD
+    export THINKINGBRIDGE_BUILD
 
     CALLED_FROM_SETUP=true BUILD_SYSTEM=build/core \
         TARGET_PRODUCT=$1 \
@@ -469,7 +469,7 @@ function print_lunch_menu()
        echo "  (ohai, koush!)"
     fi
     echo
-    if [ "z${CM_DEVICES_ONLY}" != "z" ]; then
+    if [ "z${THINKINGBRIDGE_DEVICES_ONLY}" != "z" ]; then
        echo "Breakfast menu... pick a combo:"
     else
        echo "Lunch menu... pick a combo:"
@@ -483,7 +483,7 @@ function print_lunch_menu()
         i=$(($i+1))
     done | column
 
-    if [ "z${CM_DEVICES_ONLY}" != "z" ]; then
+    if [ "z${THINKINGBRIDGE_DEVICES_ONLY}" != "z" ]; then
        echo "... and don't forget the bacon!"
     fi
 
@@ -505,10 +505,10 @@ function brunch()
 function breakfast()
 {
     target=$1
-    CM_DEVICES_ONLY="true"
+    THINKINGBRIDGE_DEVICES_ONLY="true"
     unset LUNCH_MENU_CHOICES
     add_lunch_combo full-eng
-    for f in `/bin/ls vendor/cm/vendorsetup.sh 2> /dev/null`
+    for f in `/bin/ls vendor/thinkingbridge/vendorsetup.sh 2> /dev/null`
         do
             echo "including $f"
             . $f
@@ -524,8 +524,8 @@ function breakfast()
             # A buildtype was specified, assume a full device name
             lunch $target
         else
-            # This is probably just the CM model name
-            lunch cm_$target-userdebug
+            # This is probably just the ThinkingBridge model name
+            lunch thinkingbridge_$target-userdebug
         fi
     fi
     return $?
@@ -674,7 +674,7 @@ function tapas()
 function eat()
 {
     if [ "$OUT" ] ; then
-        MODVERSION=$(get_build_var CM_VERSION)
+        MODVERSION=$(get_build_var THINKINGBRIDGE_VERSION)
         ZIPFILE=cm-$MODVERSION.zip
         ZIPPATH=$OUT/$ZIPFILE
         if [ ! -f $ZIPPATH ] ; then
@@ -690,7 +690,7 @@ function eat()
             done
             echo "Device Found.."
         fi
-    if (adb shell cat /system/build.prop | grep -q "ro.cm.device=$CM_BUILD");
+    if (adb shell cat /system/build.prop | grep -q "ro.thinkingbridge.device=$THINKINGBRIDGE_BUILD");
     then
         # if adbd isn't root we can't write to /cache/recovery/
         adb root
@@ -719,7 +719,7 @@ EOF
     fi
     return $?
     else
-        echo "The connected device does not appear to be $CM_BUILD, run away!"
+        echo "The connected device does not appear to be $THINKINGBRIDGE_BUILD, run away!"
     fi
 }
 
@@ -968,18 +968,18 @@ function qpid() {
         append='$'
         shift
     elif [ "$1" = "--help" -o "$1" = "-h" ]; then
-                echo "usage: qpid [[--exact] <process name|pid>"
-                return 255
-        fi
+    echo "usage: qpid [[--exact] <process name|pid>"              
+    return 255
+  fi
 
     local EXE="$1"
     if [ "$EXE" ] ; then
-                qpid | \grep "$prepend$EXE$append"
-        else
-                adb shell ps \
-                        | tr -d '\r' \
-                        | sed -e 1d -e 's/^[^ ]* *\([0-9]*\).* \([^ ]*\)$/\1 \2/'
-        fi
+    qpid | \grep "$prepend$EXE$append"
+  else
+    adb shell ps \
+      | tr -d '\r' \
+      | sed -e 1d -e 's/^[^ ]* *\([0-9]*\).* \([^ ]*\)$/\1 \2/'
+  fi
 }
 
 function pid()
@@ -1000,7 +1000,7 @@ function pid()
         echo "$PID"
     else
         echo "usage: pid [--exact] <process name>"
-                return 255
+    return 255  
     fi
 }
 
@@ -1560,7 +1560,7 @@ function installboot()
     sleep 1
     adb wait-for-online shell mount /system 2>&1 > /dev/null
     adb wait-for-online remount
-    if (adb shell cat /system/build.prop | grep -q "ro.cm.device=$CM_BUILD");
+    if (adb shell cat /system/build.prop | grep -q "ro.thinkingbridge.device=$THINKINGBRIDGE_BUILD");
     then
         adb push $OUT/boot.img /cache/
         for i in $OUT/system/lib/modules/*;
@@ -1576,7 +1576,7 @@ function installboot()
         adb shell chmod 644 /system/lib/modules/*
         echo "Installation complete."
     else
-        echo "The connected device does not appear to be $CM_BUILD, run away!"
+        echo "The connected device does not appear to be $THINKINGBRIDGE_BUILD, run away!"
     fi
 }
 
@@ -1610,13 +1610,13 @@ function installrecovery()
     sleep 1
     adb wait-for-online shell mount /system 2>&1 >> /dev/null
     adb wait-for-online remount
-    if (adb shell cat /system/build.prop | grep -q "ro.cm.device=$CM_BUILD");
+    if (adb shell cat /system/build.prop | grep -q "ro.thinkingbridge.device=$THINKINGBRIDGE_BUILD");
     then
         adb push $OUT/recovery.img /cache/
         adb shell dd if=/cache/recovery.img of=$PARTITION
         echo "Installation complete."
     else
-        echo "The connected device does not appear to be $CM_BUILD, run away!"
+        echo "The connected device does not appear to be $THINKINGBRIDGE_BUILD, run away!"
     fi
 }
 
@@ -1990,7 +1990,7 @@ function dopush()
         echo "Device Found."
     fi
 
-    if (adb shell cat /system/build.prop | grep -q "ro.cm.device=$CM_BUILD");
+    if (adb shell cat /system/build.prop | grep -q "ro.thinkingbridge.device=$THINKINGBRIDGE_BUILD");
     then
     # retrieve IP and PORT info if we're using a TCP connection
     TCPIPPORT=$(adb devices | egrep '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+[^0-9]+' \
@@ -2041,7 +2041,7 @@ function dopush()
     rm -f $OUT/.log
     return 0
     else
-        echo "The connected device does not appear to be $CM_BUILD, run away!"
+        echo "The connected device does not appear to be $THINKINGBRIDGE_BUILD, run away!"
     fi
 }
 
@@ -2058,7 +2058,7 @@ function repopick() {
 function fixup_common_out_dir() {
     common_out_dir=$(get_build_var OUT_DIR)/target/common
     target_device=$(get_build_var TARGET_DEVICE)
-    if [ ! -z $CM_FIXUP_COMMON_OUT ]; then
+    if [ ! -z $THINKINGBRIDGE_FIXUP_COMMON_OUT ]; then
         if [ -d ${common_out_dir} ] && [ ! -L ${common_out_dir} ]; then
             mv ${common_out_dir} ${common_out_dir}-${target_device}
             ln -s ${common_out_dir}-${target_device} ${common_out_dir}
